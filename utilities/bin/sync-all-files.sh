23faef4efd16f7125d7eafbeb3ac3f61b3377de7
Jared Ballou <github@jballou.com>
2015-12-10 14:04:10 -0500

    New data and theaters, sync file script

--- utilities/bin/sync-all-files.sh
+++ utilities/bin/sync-all-files.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 MAPSRC="rsync://ins.jballou.com/fastdl/maps/"
 MAPCYCLESRC="rsync://ins.jballou.com/fastdl/mapcycle_files/"
-HOMEDIR="/home/insserver"
+USER="insserver"
+HOMEDIR="/home/${USER}"
 INSDIR="${HOMEDIR}/serverfiles/insurgency"
 MAPDIR="${INSDIR}/maps/"
-SMDIR="${INSDIR}/addons/sourcemod"
-THEATERDIR="${INSDIR}/scripts/theaters"
-DATADIR="${INSDIR}/insurgency-data"
+BLACKLIST="${DATADIR}/thirdparty/maps-blacklist.txt"
+REPOS="addons:https://github.com/jaredballou/insurgency-addons.git addons/sourcemod:https://github.com/jaredballou/insurgency-sourcemod.git insurgency-data:https://github.com/jaredballou/insurgency-data.git scripts/theaters:https://github.com/jaredballou/insurgency-theaters.git"
 
-rsync --progress -av $MAPSRC $MAPDIR --exclude=archive
-rsync --progress -av $MAPCYCLESRC $INSDIR
-cd $SMDIR && git pull origin master
-cd $THEATERDIR && git pull origin master
-cd $DATADIR && git pull origin master
+echo "Sync Git repos"
+for REPO in $REPOS
+do
+	REPO_PATH="${INSDIR}/$(echo "${REPO}" | cut -d':' -f1)"
+	REPO_URL="$(echo "${REPO}" | cut -d':' -f2-100)"
+	PARENT="$(dirname "${REPO_PATH}")"
+	if [ ! -e "${REPO_PATH}/.git" ]
+	then
+		mkdir -p "${REPO_PATH}"
+		cd "${REPO_PATH}"
+		git init
+		git add remote origin "${REPO_URL}"
+	fi
+	cd "${REPO_PATH}"
+	git pull origin master
+	git submodule init
+	git submodule update
+done
+
+echo "Sync Maps"
+for EXT in bsp nav txt
+do
+        rsync -z --progress --ignore-existing --exclude='archive/' --exclude-from "${BLACKLIST}" "${MAPSRC}/*.${EXT}" "${MAPDIR}"
+done
+
+echo "Sync Map Cycle Files"
+rsync --progress -av "${MAPCYCLESRC}" "${INSDIR}"
+
+echo "Removing blacklisted maps"
+for MAP in $(cat "${BLACKLIST}")
+do
+        rm -vf "${MAPDIR}${MAP}"
+done
 
